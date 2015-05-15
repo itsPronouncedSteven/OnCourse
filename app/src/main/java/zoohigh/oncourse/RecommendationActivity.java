4db83db1547f148ae59f4948e20701488f11d59a
kammalage <shanantonfernando@gmail.com>
2015-05-15 02:12:34 -0700

    added icon

--- app/src/main/java/zoohigh/oncourse/RecommendationActivity.java
+++ app/src/main/java/zoohigh/oncourse/RecommendationActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -68,6 +69,7 @@ public class RecommendationActivity extends ActionBarActivity {
                 }
         );
         */
+
     }
 
 
@@ -114,8 +116,6 @@ public class RecommendationActivity extends ActionBarActivity {
 
             return view;
         }
-
-
 
     }
     */
